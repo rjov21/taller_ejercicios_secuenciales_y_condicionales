a5adaf95a11ed6f1fa2983724430593640c51523
Robinson Ordoñez <robinsonor4@gmail.com>
2021-02-28 17:25:45 -0500

    ejercicio_1

--- taller_condicionales.py
+++ taller_condicionales.py
@@ -5,3 +5,12 @@ Created on Sun Feb 28 16:57:19 2021
 @author: DIOS BENDITO
 """
 
+# ejercicio 1
+cantidad = int(input('ingrese la cantidad de camisetas: '))
+valor = int(input('ingrese el valor de las camisetas: '))
+if cantidad >= 3:
+    descuento = (valor * cantidad) * 0.3
+else:
+    descuento = (valor * cantidad) * 0.1
+monto_final = (valor * cantidad) - descuento
+print(f'el monto final a pagar es: {monto_final}')
