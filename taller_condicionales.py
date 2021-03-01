872e58e5ca9438a221e5f65e3ef5421843823b17
Robinson Ordoñez <robinsonor4@gmail.com>
2021-02-28 19:37:13 -0500

    ejercicio_4

--- taller_condicionales.py
+++ taller_condicionales.py
@@ -1,11 +1,11 @@
 # ejercicio 1
-cantidad = int(input('ingrese la cantidad de camisetas'))
-valor = int(input('ingrese el valor de camiseta'))
+cantidad = int(input('ingrese la cantidad de camisetas: '))
+valor = int(input('ingrese el valor de camiseta: '))
 if cantidad >= 3:
     descuento = (valor * cantidad) * 0.3
 else:
     descuento = (valor * cantidad) * 0.1
-valor_final = valor - descuento
+valor_final = (valor * cantidad) - descuento
 print(f'el valor final a pagar es: {valor_final}')
 
 # ejercicio 2
@@ -25,3 +25,12 @@ if monto < 50000:
 else:
     cuota = monto * 0.02
 print(f'la cuota que debe pagar la afianzadora es: {cuota}')
+
+# ejercicio 4
+promedio_puntos = int(input('ingrese el promedio de puntos de la semana: '))
+ganancia_dia = int(input('ingrese las ganacias darias: '))
+if promedio_puntos >170:
+    perdida = (ganancia_dia * 5) * 0.5
+else:
+    print('no habra perdida')
+print(f'la fabrica perdera {perdida} en la semana ')
